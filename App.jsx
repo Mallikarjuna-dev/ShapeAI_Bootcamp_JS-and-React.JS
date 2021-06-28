52754be9bc706b36eae77fda381b6ce92b72724d
Mallikarjuna Annigeri <76250603+Mallikarjuna-dev@users.noreply.github.com>
2021-06-28 09:46:46 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Info from "./Info";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Info />
+      <Info />
+      <Info />
+      <Info />
+      <Info />
+      <Info />
+      <Info />
+      <Info />
+      <Footer />
+    </div>
+  );
+}
+export default App;
---
+++ Info.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Info(props) {
+  return (
+    <div className="note">
+      <h1>Javascript and React.JS</h1>
+      <p>a basic web development React.Js bootcamt</p>
+    </div>
+  );
+}
+export default Info;
